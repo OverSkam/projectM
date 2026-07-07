f6a2ca199a0678e76fab2fb351d46d94adcda8ad
Martynenko Maksym <125035785+OverSkam@users.noreply.github.com>
2026-07-07 14:47:52 +0300

    fix: UUIDv7

--- src/main/java/overskam/projectM/model/AbstractModel.java
+++ src/main/java/overskam/projectM/model/AbstractModel.java
@@ -3,6 +3,7 @@ package overskam.projectM.model;
 import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.UuidGenerator;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -16,7 +17,8 @@ import java.util.UUID;
 @NoArgsConstructor
 public class AbstractModel {
     @Id
-    @GeneratedValue(strategy = GenerationType.UUID)
+    @UuidGenerator(style = UuidGenerator.Style.VERSION_7)
+    @Column(name = "id", nullable = false, updatable = false)
     private UUID id;
 
     @CreatedDate
